bb5c1e750425486b528d71439f292e0f70b234d2
Shiv Kumar <shivkumar4883@gmail.com>
2025-08-30 20:35:26 +0530

    not sure what is the change

--- hello.sh
+++ hello.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "Hello, Devops"
+
+# write a greeting
+
+echo "Hello, $(whoami)"
+
+# check execution permission for owner
+if [ -x "$0" ]; then
+ echo "you have execute permission on this script"
+else 
+ echo "Warning: you don't have execute permission"
+fi
+
